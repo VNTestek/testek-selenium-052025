3f9d3840d0127dcd42907d8c049db162577e98e9
Huong Quynh <quynhhuongdo312@gmail.com>
2025-05-18 23:07:09 +0700

    HuongQuynh nop BTVN02

--- src/test/java/com/testek/study/lesson02/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson02/exercise/SolutionTest.java
@@ -49,6 +49,62 @@ public class SolutionTest {
         WebElement lblWebsiteEle = webDriver.findElement(By.cssSelector(lblWebsiteCSS));
 
         // Handle other elements similarly
+        //h4[name='db-title-name']
+        String lblWebsiteTitleCSS = "h4[name='db-title-name']";
+        WebElement lblTitle = webDriver.findElement(By.cssSelector(lblWebsiteTitleCSS));
+
+        //h4[test-id='db-website']
+        String lblDbWebsiteCSS = "h4[test-id='db-website']";
+        WebElement lblDBTitle = webDriver.findElement(By.cssSelector(lblDbWebsiteCSS));
+
+        //h4[test-id='db-discord']
+        String lblDbDiscordCSS = "h4[test-id='db-discord']";
+        WebElement lblDBDiscord = webDriver.findElement(By.cssSelector(lblDbDiscordCSS));
+
+        //h4[test-id='db-facebook']
+        String lblDbFacebookCSS = "h4[test-id='db-facebook']";
+        WebElement lblFacebook = webDriver.findElement(By.cssSelector(lblDbFacebookCSS));
+
+        //h4[test-id='db-contact']
+        String lblDbContactCSS = "h4[test-id='db-contact']";
+        WebElement lblContact = webDriver.findElement(By.cssSelector(lblDbContactCSS));
+
+        //h4[test-id='db-systems']
+        String lblDbSystemCSS = "h4[test-id='db-systems']";
+        WebElement lblSystem = webDriver.findElement(By.cssSelector(lblDbSystemCSS));
+
+        //li[test-id='db-api-system']
+        String lblDbApiCSS = "li[test-id='db-api-system']";
+        WebElement lblApi = webDriver.findElement(By.cssSelector(lblDbApiCSS));
+
+        //li[test-id='db-automation-web']
+        String lblAutoWebCSS = "li[test-id='db-automation-web']";
+        WebElement lblAuto = webDriver.findElement(By.cssSelector(lblAutoWebCSS));
+
+        //li[test-id='db-web-element']
+        String lblWebElementCSS = "li[test-id='db-web-element']";
+        WebElement lblWebElement = webDriver.findElement(By.cssSelector(lblWebElementCSS));
+
+        //li[test-id='db-account']
+        String lblAccountCSS = "li[test-id='db-account']";
+        WebElement lblAccount = webDriver.findElement(By.cssSelector(lblAccountCSS));
+
+        //li[test-id='db-database']
+        String lblDatabaseCSS = "li[test-id='db-database']";
+        WebElement lblDatabase = webDriver.findElement(By.cssSelector(lblDatabaseCSS));
+
+        //img[test-id='about-me-coffee-img']
+        String lblImgCSS = "img[test-id='about-me-coffee-img']";
+        WebElement lblImg = webDriver.findElement(By.cssSelector(lblImgCSS));
+
+        //h4[test-id='about-me-web-elements']
+        String lblAboutMeCSS = "h4[test-id='about-me-web-elements']";
+        WebElement lblAboutMe = webDriver.findElement(By.cssSelector(lblAboutMeCSS));
+
+        //div[data-e2e='playlist-playlistCard-Container'] -> if keep line code, when running test, system will notice as 'test failed'
+//        String lblPlaylistCSS = "div[data-e2e='playlist-playlistCard-Container']";
+//        WebElement lblPlaylist = webDriver.findElement(By.cssSelector(lblPlaylistCSS));
+
 
     }
 
@@ -61,6 +117,53 @@ public class SolutionTest {
         WebElement lblWebsiteEle = webDriver.findElement(By.xpath(lblWebsiteXPath));
 
         // Handle other elements similarly
+        //h4[@name='db-title-name']
+        String lblWebsiteTitleXPath = "//h4[@name='db-title-name']";
+        WebElement lblTitle = webDriver.findElement(By.xpath(lblWebsiteTitleXPath));
+
+        //h4[@test-id='db-discord']
+        String lblDbDiscordXPath = "//h4[@test-id='db-discord']";
+        WebElement lblDBDiscord = webDriver.findElement(By.xpath(lblDbDiscordXPath));
+
+        //h4[@test-id='db-facebook']
+        String lblDbFacebookXPath = "//h4[@test-id='db-facebook']";
+        WebElement lblFacebook = webDriver.findElement(By.xpath(lblDbFacebookXPath));
+
+        //h4[@test-id='db-contact']
+        String lblDbContactXPath = "//h4[@test-id='db-contact']";
+        WebElement lblContact = webDriver.findElement(By.xpath(lblDbContactXPath));
+
+        //h4[@test-id='db-systems']
+        String lblDbSystemXPath = "//h4[@test-id='db-systems']";
+        WebElement lblSystem = webDriver.findElement(By.xpath(lblDbSystemXPath));
+
+        //li[@test-id='db-api-system']
+        String lblDbApiXPath = "//li[@test-id='db-api-system']";
+        WebElement lblApi = webDriver.findElement(By.xpath(lblDbApiXPath));
+
+        //li[@test-id='db-automation-web']
+        String lblAutoWebXpath = "//li[@test-id='db-automation-web']";
+        WebElement lblAuto = webDriver.findElement(By.xpath(lblAutoWebXpath));
+
+        //li[@test-id='db-web-element']
+        String lblDbWebElementXPath = "//li[@test-id='db-web-element']";
+        WebElement lblDbWebElement = webDriver.findElement(By.xpath(lblDbWebElementXPath));
+
+        //li[@test-id='db-account']
+        String lblDbAccountXPath = "//li[@test-id='db-account']";
+        WebElement lblDbAccount = webDriver.findElement(By.xpath(lblDbAccountXPath));
+
+        //li[@test-id='db-database']
+        String lblDbDatabaseXPath = "//li[@test-id='db-database']";
+        WebElement lblDbDatabase = webDriver.findElement(By.xpath(lblDbDatabaseXPath));
+
+        //img[@test-id='about-me-coffee-img']
+        String lblImageXPath = "//img[@test-id='about-me-coffee-img']";
+        WebElement lblImage = webDriver.findElement(By.xpath(lblImageXPath));
+
+        //h4[@test-id='about-me-web-elements']
+        String lblAboutMeXPath = "//h4[@test-id='about-me-web-elements']";
+        WebElement lblAboutMe = webDriver.findElement(By.xpath(lblAboutMeXPath));
 
     }
 
@@ -72,4 +175,5 @@ public class SolutionTest {
         String url = "https://testek.vn/lab/auto/web-elements/";
         webDriver.get(url);
     }
+
 }
