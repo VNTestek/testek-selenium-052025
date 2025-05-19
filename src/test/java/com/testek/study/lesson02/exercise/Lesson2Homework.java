77e7587e2c8026de32e674ef8b16131851b3b83a
hoangnhathai <hoangnhathai.95@gmail.com>
2025-05-20 01:01:13 +0700

    Hoang Nhat Hai updates Lession1

--- src/test/java/com/testek/study/lesson02/exercise/Lesson2Homework.java
+++ src/test/java/com/testek/study/lesson02/exercise/Lesson2Homework.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.Objects;
 
@@ -16,29 +19,37 @@ import java.util.Objects;
 public class Lesson2Homework {
     private WebDriver webDriver;
 
+    @BeforeClass
     public void beforeClass() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--max-window-size");
         chromeOptions.addArguments("--remote-allow-origins=*");
         webDriver = new ChromeDriver(chromeOptions);
-        webDriver.manage().window().maximize();}
-        public void afterClass () {
-            if (Objects.nonNull(webDriver)) webDriver.quit();}
+        webDriver.manage().window().maximize();
+    }
 
+    public void afterClass() {
+        if (Objects.nonNull(webDriver)) webDriver.quit();
+    }
+
+    @AfterClass
     public void findLocatorWithCSS() {
         gotoTestWebsite();
         String lblWebsiteCSS = "h3[test-id='student-registration-title'";
         WebElement lblWebsiteEle = webDriver.findElement(By.cssSelector(lblWebsiteCSS));
 
     }
-    //public void findLocatorWithXPath() {
-        //gotoTestWebsite();
 
-      //  String lblWebsiteXPath = "//h4[@test-id='db-website']";
-      //  WebElement lblWebsiteEle = webDriver.findElement(By.xpath(lblWebsiteXPath));
+    @Test
+    public void findLocatorWithXPath() {
+        gotoTestWebsite();
+
+        String lblWebsiteXPath = "input[@placeholder='Enter your first name']";
+        WebElement lblWebsiteEle = webDriver.findElement(By.xpath(lblWebsiteXPath));
 
 
-    //}
+    }
+
     private void gotoTestWebsite() {
         String url = "https://testek.vn/lab/auto/web-elements/";
         webDriver.get(url);
