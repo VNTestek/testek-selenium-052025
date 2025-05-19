9d7fc3cb95198c555663daf8c1e1611be26e2b84
kiendoan796 <kiendoan796@gmail.com>
2025-05-19 23:52:01 +0700

    [kiendoan796]: Nop BTVN 02

--- src/test/java/com/testek/study/lesson02/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson02/exercise/SolutionTest.java
@@ -49,7 +49,73 @@ public class SolutionTest {
         WebElement lblWebsiteEle = webDriver.findElement(By.cssSelector(lblWebsiteCSS));
 
         // Handle other elements similarly
+        // Title TESTEK - KIỂM THỬ THỰC CHIẾN
+        lblWebsiteCSS= "h2>b";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
 
+        // Đào tạo API Testing, Performance Testing, Automation Testing
+        lblWebsiteCSS= "#db-title-id";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+        lblWebsiteCSS= "h4.db-title-class";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+        lblWebsiteCSS= "h4[test-id='db-title']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        //Link Website
+        lblWebsiteCSS= "a[href='https://testek.vn']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        // Link Discord
+        lblWebsiteCSS= "a[href='https://discord.gg/jC7ZtmvuDX']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+        lblWebsiteCSS= "a[test-id='db-discord-link']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        // Link Facebook
+        lblWebsiteCSS= "a[href='https://www.facebook.com/share/g/15m67fr73k/']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+        lblWebsiteCSS= "a[test-id='db-facebook-link']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        // Thông tin liên hệ SĐT email
+        lblWebsiteCSS= "h4[test-id='db-contact']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        // Các hệ thống Testek cung cấp
+        lblWebsiteCSS= "h4[test-id='db-systems']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        // API System
+        lblWebsiteCSS= "a[test-id='db-api-system-link']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+        lblWebsiteCSS= "a[href='https://testek.vn/lab/api/v0/prod-man/swagger-ui.html#/']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        // Automation Web
+        lblWebsiteCSS= "a[test-id='db-automation-web-link']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+        lblWebsiteCSS= "a[href='https://testek.vn/lab/auto/login']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        // Web Element
+        lblWebsiteCSS= "a[test-id='db-web-element-link']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+        lblWebsiteCSS= "a[href='https://testek.vn/lab/auto/web-elements']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+//        // Tiktok
+//        lblWebsiteCSS= "div.css-1f603so e1v8cfre1";
+//        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        // Buy me a coffee
+        lblWebsiteCSS= "p[test-id='about-me-coffee-text']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+
+        // Image
+        lblWebsiteCSS= "img[src='https://lh3.googleusercontent.com/d/1jomM5CqmS7JPuI9docJdnBeYil5vR_yR=s220']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
+        lblWebsiteCSS= "img[test-id='about-me-coffee-img']";
+        webDriver.findElement(By.cssSelector(lblWebsiteCSS));
     }
 
     @Test(description = "Find element with XPath")
@@ -61,6 +127,48 @@ public class SolutionTest {
         WebElement lblWebsiteEle = webDriver.findElement(By.xpath(lblWebsiteXPath));
 
         // Handle other elements similarly
+        // Title TESTEK - KIỂM THỬ THỰC CHIẾN
+        lblWebsiteXPath = "//b['TESTEK - KIỂM THỬ THỰC CHIẾN']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+
+        // Đào tạo API Testing, Performance Testing, Automation Testing
+        lblWebsiteXPath= "//h4[@id='db-title-id']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+        lblWebsiteXPath= "//h4[@name='db-title-name']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+        lblWebsiteXPath= "//h4[@class='db-title-class']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+
+        //Link Website
+        webDriver.findElement(By.partialLinkText("testek.vn"));
+
+        // Link Discord
+        lblWebsiteXPath= "//a[@href='https://discord.gg/jC7ZtmvuDX']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+
+        // Link Facebook
+        lblWebsiteXPath= "//a[@href='https://www.facebook.com/share/g/15m67fr73k/']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+
+        // Thông tin liên hệ SĐT email
+        lblWebsiteXPath= "//h4[@test-id='db-contact']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+
+        // Các hệ thống Testek cung cấp
+        lblWebsiteXPath= "//h4[@test-id='db-systems']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+
+        // API System
+        lblWebsiteXPath= "//a[@href='https://testek.vn/lab/api/v0/prod-man/swagger-ui.html#/']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+
+        // Automation Web
+        lblWebsiteXPath= "//a[@href='https://testek.vn/lab/auto/login']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
+
+        // Web Element
+        lblWebsiteXPath= "//a[@test-id='db-web-element-link']";
+        webDriver.findElement(By.xpath(lblWebsiteXPath));
 
     }
 
