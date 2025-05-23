0f06c5dd2281e26b76fc349a2749311ff01aa58a
vietdung385 <nguyendung5112004@gmail.com>
2025-05-23 11:36:54 +0700

    Em nop Btvn a.

--- src/test/java/com/testek/study/lesson03/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson03/exercise/SolutionTest.java
@@ -18,6 +18,7 @@ import java.util.Objects;
 public class SolutionTest {
     // The driver for interacting with the webpage
     private WebDriver webDriver;
+    private WebElement webElement;
 
     /**
      * Method will be executed before each class, configure Chrome Driver and initialize
@@ -48,6 +49,64 @@ public class SolutionTest {
 
         // Handle other elements similarly
 
+        String containsTitleCSS="[test-id*='about-me-title']";
+        webElement =webDriver.findElement(By.cssSelector(containsTitleCSS));
+        String startTitleCSS="[test-id^='about-me-title']";
+        webElement =webDriver.findElement(By.cssSelector(containsTitleCSS));
+        String lastTitleCSS="[test-id$='about-me-title']";
+        webElement =webDriver.findElement(By.cssSelector(containsTitleCSS));
+
+        String containsWebsiteCSS="[test-id*='db-website']";
+        webElement =webDriver.findElement(By.cssSelector(containsWebsiteCSS));
+        String startWebsiteCSS="[test-id^='db-website']";
+        webElement =webDriver.findElement(By.cssSelector(containsWebsiteCSS));
+        String lastWebsiteCSS="[test-id$='db-link']";
+        webElement =webDriver.findElement(By.cssSelector(containsWebsiteCSS));
+
+        String containsDiscordCss="[test-id*='db-discord']";
+        webElement = webDriver.findElement(By.cssSelector(containsDiscordCss));
+        String startDiscordCss="[id^='db-discord']";
+        webElement = webDriver.findElement(By.cssSelector(containsDiscordCss));
+        String lastDiscordCss="[id$='db-discord']";
+        webElement = webDriver.findElement(By.cssSelector(containsDiscordCss));
+
+        String containsFacebookCSS="[test-id*='db-facebook']";
+        webElement=webDriver.findElement(By.cssSelector(containsFacebookCSS));
+        String startFacebookCSS="[test-id^='db-facebook']";
+        webElement=webDriver.findElement(By.cssSelector(containsFacebookCSS));
+        String lastFacebookCSS="[test-id$='db-facebook']";
+        webElement=webDriver.findElement(By.cssSelector(containsFacebookCSS));
+
+        String containsContactCSS="[test-id*='db-contact']";
+        webElement=webDriver.findElement(By.cssSelector(containsContactCSS));
+        String startContactCSS="[test-id^='db-contact']";
+        webElement=webDriver.findElement(By.cssSelector(containsContactCSS));
+        String lastContactCSS="[test-id$='db-contact']";
+        webElement=webDriver.findElement(By.cssSelector(containsContactCSS));
+
+        String containSystemCSS="[test-id*='db-system']";
+        webElement=webDriver.findElement(By.cssSelector(containSystemCSS));
+        String startSystemCSS="[test-id^='db-system']";
+        webElement=webDriver.findElement(By.cssSelector(containSystemCSS));
+        String lastSystemCSS="[test-id$='db-system']";
+        webElement=webDriver.findElement(By.cssSelector(containSystemCSS));
+
+        String containApiCSS="[test-id*='db-api-system']";
+        webElement=webDriver.findElement(By.cssSelector(containApiCSS));
+        String startApiCSS="[test-id^='db-api-system']";
+        webElement=webDriver.findElement(By.cssSelector(containApiCSS));
+        String lastApiCSS="[test-id$='db-api-system']";
+        webElement=webDriver.findElement(By.cssSelector(containApiCSS));
+
+        String containAccountCSS="[test-id*='system'] li[test-id*='db-account']";
+        webElement=webDriver.findElement(By.cssSelector(containAccountCSS));
+        String startAccountCSS="[test-id^='system'] li[test-id*='db-account']";
+        webElement=webDriver.findElement(By.cssSelector(containAccountCSS));
+        String lastAccountCSS="[test-id$='system'] li[test-id*='db-account']";
+        webElement=webDriver.findElement(By.cssSelector(containAccountCSS));
+
+
+
     }
 
     /**
@@ -62,6 +121,55 @@ public class SolutionTest {
         WebElement lblTitleElement = webDriver.findElement(By.xpath(lblTitleAncestorXPath));
 
         // Handle other elements similarly
+
+        String containsTitleXPATH="//h2[contains(@test-id,'about-me')";
+        webElement=webDriver.findElement(By.xpath(containsTitleXPATH));
+        String startTitleXPATH="//h2[start-with(@test-id,'about-']";
+        webElement = webDriver.findElement(By.xpath(startTitleXPATH));
+        String descendantTitle="//div[@test-id='about-me-grid']/descendant::h2";
+        webElement=webDriver.findElement(By.xpath(descendantTitle));
+
+        String containsWebsiteXpath="//h4[contains(@name,'name']";
+        webElement=webDriver.findElement(By.xpath(containsWebsiteXpath));
+        String startWebsiteXpath="h4[star-with(@name,'db-title)']";
+        webElement=webDriver.findElement(By.xpath(startWebsiteXpath));
+        String textWebsiteXpath="//h4[text()='Đào tạo API Testing,Performance Testing,Automation Testing']";
+        webElement=webDriver.findElement(By.xpath(textWebsiteXpath));
+
+        String containsDiscordXpath="//h4[contains(@test-id,'db-discord')]";
+        webElement=webDriver.findElement(By.xpath(containsDiscordXpath));
+        String startDiscordXpath="//h4[start-with(@test-id,'db-disco')]";
+        webElement=webDriver.findElement(By.xpath(startDiscordXpath));
+        String startDiscordLinkXpath="//[start-with(@href,'https://discord.gg/')]";
+        webElement=webDriver.findElement(By.xpath(startDiscordLinkXpath));
+
+        String containsFbXpath="//h4[contains(@test-id,'facebook')]";
+        webElement=webDriver.findElement(By.xpath(containsFbXpath));
+        String startFbXpath="//h4[start-with(@test-id,'db-facebook')]";
+        webElement=webDriver.findElement(By.xpath(startFbXpath));
+        String containsFbLinkXpath="//[contains(@href,'facebook.com']";
+        webElement=webDriver.findElement(By.xpath(containsFbLinkXpath));
+
+        String containsContactXpath="//[contains(@test-id,'db-contact')]";
+        webElement=webDriver.findElement(By.xpath(containsContactXpath));
+        String startContactXpath="//[starts-with(@test-id,'db-contact')]";
+        webElement=webDriver.findElement(By.xpath(startContactXpath));
+        String textContactXpath="//h4[text()='Hotline:083.286.8822/Email: vntestek@gmail.com']";
+        webElement=webDriver.findElement(By.xpath(textContactXpath));
+
+        String containsSystemXpath="//h4[contain(@test-id,db-systems')]";
+        webElement=webDriver.findElement(By.xpath(containsSystemXpath));
+        String startSystemXpath="//h4[start-with(@test-id,db-systems')]";
+        webElement=webDriver.findElement(By.xpath(startSystemXpath));
+        String childSystemXpath="//div[@test-id='about-me-grid-item']/child::h4[@test-id='db-system']";
+        webElement=webDriver.findElement(By.xpath(childSystemXpath));
+
+
+
+
+
+
+
     }
 
 
