5a9a83124dc453806061a6dae3291199fcbbf088
lienltb <lienltb8@gmail.com>
2025-05-24 05:36:32 +0700

    [Lienltb8] Lam bai tap lesson 3

--- src/test/java/com/testek/study/lesson03/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson03/exercise/SolutionTest.java
@@ -47,6 +47,13 @@ public class SolutionTest {
         gotoTestWebsite();
 
         // Handle other elements similarly
+        // Page Title:
+        String lblTitleContainsCss = "h4[id*='db-title-id']";
+        WebElement lblTitleElement = webDriver.findElement((By.cssSelector(lblTitleContainsCss)));
+
+        //Image: By Me a Cooffe
+        String imgQrStarsWithCss = "img[test-id^='about-me']";
+        WebElement imgQrElement = webDriver.findElement((By.cssSelector(imgQrStarsWithCss)));
 
     }
 
@@ -62,6 +69,31 @@ public class SolutionTest {
         WebElement lblTitleElement = webDriver.findElement(By.xpath(lblTitleAncestorXPath));
 
         // Handle other elements similarly
+
+        //Page Title
+        String lblPageTitleAncestorXpath = "//h4[@test-id='db-title']/ancestor::div[@id='about-me']//h2";
+        WebElement lblPageTitleElement = webDriver.findElement((By.xpath(lblPageTitleAncestorXpath)));
+
+        // Description: Đào tạo API Testing, Performance Testing, Automation Testing
+        String lblDescriptionPrecedingXpath = "//h4[@test-id='db-website']/preceding::h4[@id='db-title-id']";
+        WebElement lbDescriptionElement = webDriver.findElement((By.xpath(lblDescriptionPrecedingXpath)));
+
+        // Facebook:
+        String lblFacebookFollowingXpath = "//h4[@test-id='db-website']/following::h4[@test-id='db-facebook']";
+        WebElement lblFacebookElement = webDriver.findElement((By.xpath(lblFacebookFollowingXpath)));
+
+        // System: Các hệ thống testek cung cấp
+        String lblSystemTextXpath = "//h4[text()='Các hệ thống Testek cung cấp']";
+        WebElement lblSystemElement = webDriver.findElement((By.xpath(lblSystemTextXpath)));
+
+        // Account: for API System & Automation Web
+        String lblAccountContainsXpath = "//li[@test-id='db-account-admin']/ancestor::li[contains(@test-id, 'account')]/b";
+        WebElement lblAccountElement = webDriver.findElement((By.xpath(lblAccountContainsXpath)));
+
+        //Image: By Me a Cooffe
+        String imgQrStarsWithXpath = "//img[starts-with(@test-id, 'about-me')]";
+        WebElement imgQrElement = webDriver.findElement((By.xpath(imgQrStarsWithXpath)));
+
     }
 
 
