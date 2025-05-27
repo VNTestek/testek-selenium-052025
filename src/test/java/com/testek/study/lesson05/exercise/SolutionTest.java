1df006bdbb08663c0a6495e6370f5afa468f1cf2
huongquynh <quynhhuongdo312@gmail.com>
2025-05-27 23:54:06 +0700

    huongquynh nop bai thuc hanh 05

--- src/test/java/com/testek/study/lesson05/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson05/exercise/SolutionTest.java
@@ -47,6 +47,107 @@ public class SolutionTest {
         gotoTestWebsite();
 
         // Handle other elements similarly
+        //Form đăng ký sinh viên
+        //h3[@test-id='textbox-form-output-title']
+        String lblTtlXPath = "//h3[@test-id='textbox-form-output-title']";
+        WebElement lblTtlEle = webDriver.findElement(By.xpath(lblTtlXPath));
+
+        //First Name:
+        //label[@test-id='label-firstName']
+        String lblFirstNameFieldXPath = "//label[@test-id='label-firstName']";
+        WebElement firstNameFieldEle = webDriver.findElement(By.xpath(lblFirstNameFieldXPath));
+        //input[@placeholder='Nhập họ tên']
+        String lblFirstNamePlaceholderXPath = "//input[@placeholder='Nhập họ tên']";
+        WebElement firstNamPlaceholderEle = webDriver.findElement(By.xpath(lblFirstNamePlaceholderXPath));
+
+        //label[@test-id='label-lastName']
+        String lblLastNameFieldXPath = "//label[@test-id='label-lastName']";
+        WebElement lastNameFieldEle = webDriver.findElement(By.xpath(lblLastNameFieldXPath));
+        //input[@placeholder='Enter your last name']
+        String lblLastNamePlaceholderXPath = "//input[@placeholder='Enter your last name']";
+        WebElement lastNamePlaceholderEle = webDriver.findElement(By.xpath(lblLastNamePlaceholderXPath));
+
+        //Email:
+        //label[@test-id='label-email']
+        String lblEmailFieldXPath = "//label[@test-id='label-email']";
+        WebElement emailFieldEle = webDriver.findElement(By.xpath(lblEmailFieldXPath));
+        //input[@placeholder='Enter your email']
+        String lblEmailPlaceholderXPath = "//input[@placeholder='Enter your email']";
+        WebElement emailPlaceholderEle = webDriver.findElement(By.xpath(lblEmailPlaceholderXPath));
+
+        //Gender:
+        //label[@test-id='label-gender']
+        String lblGenderFieldXPath = "//label[@test-id='label-gender']";
+        WebElement genderFieldEle = webDriver.findElement(By.xpath(lblGenderFieldXPath));
+        //input[@id='male']
+        String rdoMaleXPath = "//input[@id='male']";
+        WebElement rdoMaleEle = webDriver.findElement(By.xpath(rdoMaleXPath));
+        //input[@id='female']
+        String rdoFemaleXPath = "//input[@id='female']";
+        WebElement rdoFemaleEle = webDriver.findElement(By.xpath(rdoFemaleXPath));
+
+        //Mobile:
+        //label[@test-id='label-mobile']
+        String lblMobileFieldXPath = "//label[@test-id='label-mobile']";
+        WebElement mobileFieldEle = webDriver.findElement(By.xpath(lblMobileFieldXPath));
+        //input[@placeholder='Enter your mobile number']
+        String lblMobilePlaceholderXPath = "//input[@placeholder='Enter your mobile number']";
+        WebElement mobilePlaceholderEle = webDriver.findElement(By.xpath(lblMobilePlaceholderXPath));
+
+        //Date of Birth:
+        //label[@test-id='label-dob']
+        String lblDobFieldXPath = "//label[@test-id='label-dob']";
+        WebElement dobFieldEle = webDriver.findElement(By.xpath(lblDobFieldXPath));
+        //input[@type='date']
+        String lblDatePickerXPath = "//input[@type='date']";
+        WebElement datePickerEle = webDriver.findElement(By.xpath(lblDatePickerXPath));
+
+        //Subjects:
+        //label[@test-id='label-subjects']
+        String lblSubjectsFieldXPath = "//label[@test-id='label-subjects']";
+        WebElement subjectsFieldEle = webDriver.findElement(By.xpath(lblSubjectsFieldXPath));
+        //input[@placeholder='Enter your subjects']
+        String lblSbjPlaceholderXPath = "//input[@placeholder='Enter your subjects']";
+        WebElement subjectsPlaceholderEle = webDriver.findElement(By.xpath(lblSbjPlaceholderXPath));
+
+        //Hobbies:
+        //label[@test-id='label-hobbies']
+        String lblHobbiesFieldXPath = "//label[@test-id='label-hobbies']";
+        WebElement hobbiesFieldEle = webDriver.findElement(By.xpath(lblHobbiesFieldXPath));
+        //input[@id='reading']
+        String chkReadingXPath = "//input[@id='reading']";
+        WebElement chkReadingEle = webDriver.findElement(By.xpath(chkReadingXPath));
+        //input[@id='sports']
+        String chkSportsXPath = "//input[@id='sports']";
+        WebElement chkSportsEle = webDriver.findElement(By.xpath(chkSportsXPath));
+        //input[@id='music']
+        String chkMusicXPath = "//input[@id='music']";
+        WebElement chkMusicEle = webDriver.findElement(By.xpath(chkMusicXPath));
+
+        //Address:
+        //label[@test-id='label-address']
+        String lblAddressXPath = "//label[@test-id='label-address']";
+        WebElement addressFieldEle = webDriver.findElement(By.xpath(lblAddressXPath));
+        //textarea[@placeholder='Enter your address']
+        String textareaXPath = "//textarea[@placeholder='Enter your address']";
+        WebElement textareaEle = webDriver.findElement(By.xpath(textareaXPath));
+
+        //Buttons
+        //button[@id='submit-button']
+        String btnSubmitXPath = "//button[@id='submit-button']";
+        WebElement btnSubmitEle = webDriver.findElement(By.xpath(btnSubmitXPath));
+        //button[@id='reset-button']
+        String btnClearXPath = "//button[@id='reset-button']";
+        WebElement btnClearEle = webDriver.findElement(By.xpath(btnClearXPath));
+
+        //Thông tin bạn đã nhập:
+        //h3[@test-id='submitted-info-title']
+        String lblInputInfoFieldXPath = "//h3[@test-id='submitted-info-title']";
+        WebElement inputInfoFieldEle = webDriver.findElement(By.xpath(lblInputInfoFieldXPath));
+        //textarea[@test-id='outputTextStudent']
+        String textareaInfoFieldXPath = "//textarea[@test-id='outputTextStudent']";
+        WebElement textareaInfoFieldEle = webDriver.findElement(By.xpath(textareaInfoFieldXPath));
+
 
     }
 
@@ -55,6 +156,7 @@ public class SolutionTest {
         gotoTestWebsite();
 
         // Handle other elements similarly
+
 
     }
 
@@ -67,6 +169,52 @@ public class SolutionTest {
         gotoTestWebsite();
 
         // Define the locator format then apply to elements
+        //input[@type and @placeholder]
+        String FORM_XPATH_INPUT_PLACEHOLDER = "//input[@type='%s' and @placeholder='%s']";
+        String lblFirstNamePlaceholderXPath = String.format(FORM_XPATH_INPUT_PLACEHOLDER, "text", "Enter your first name");
+        String lblLastNamePlaceholderXPath = String.format(FORM_XPATH_INPUT_PLACEHOLDER, "text", "Enter your last name");
+        String lblEmailPlaceholderXPath = String.format(FORM_XPATH_INPUT_PLACEHOLDER, "email", "Enter your email");
+        String lblMobilePlaceholderXPath = String.format(FORM_XPATH_INPUT_PLACEHOLDER, "text", "Enter your mobile number");
+        String lblSbjPlaceholderXPath = String.format(FORM_XPATH_INPUT_PLACEHOLDER, "text", "Enter your subjects");
+        WebElement lblFirstNameEle = webDriver.findElement(By.xpath(lblFirstNamePlaceholderXPath));
+        WebElement lblLastNameEle = webDriver.findElement(By.xpath(lblLastNamePlaceholderXPath));
+        WebElement lblEmailEle = webDriver.findElement(By.xpath(lblEmailPlaceholderXPath));
+        WebElement lblMobileEle = webDriver.findElement(By.xpath(lblMobilePlaceholderXPath));
+        WebElement lblSbjEle = webDriver.findElement(By.xpath(lblSbjPlaceholderXPath));
+
+        //input[@type and @id and @name]
+        String FORM_BUTTON_SELECT_XPATH_TYPE = "//input[@type='%s' and @id='%s' and @name='%s']";
+        String rdoGenderMXPath = String.format(FORM_BUTTON_SELECT_XPATH_TYPE, "radio", "male", "gender");
+        String rdoGenderFXPath = String.format(FORM_BUTTON_SELECT_XPATH_TYPE, "radio", "female", "gender");
+        String chkReadingXPath = String.format(FORM_BUTTON_SELECT_XPATH_TYPE, "checkbox", "reading", "hobbies");
+        String chkSportsXPath = String.format(FORM_BUTTON_SELECT_XPATH_TYPE, "checkbox", "sports", "hobbies");
+        String chkMusicXPath = String.format(FORM_BUTTON_SELECT_XPATH_TYPE, "checkbox", "music", "hobbies");
+        WebElement rdoGenderMEle = webDriver.findElement(By.xpath(rdoGenderMXPath));
+        WebElement rdoGenderFEle = webDriver.findElement(By.xpath(rdoGenderFXPath));
+        WebElement chkReadingMEle = webDriver.findElement(By.xpath(chkReadingXPath));
+        WebElement chkSportsMEle = webDriver.findElement(By.xpath(chkSportsXPath));
+        WebElement chkMusicMEle = webDriver.findElement(By.xpath(chkMusicXPath));
+
+        //input[@id]
+        String FORM_XPATH_DOB = "//input[@id='%s']";
+        String lblDobXPath = String.format(FORM_XPATH_DOB, "dob");
+        WebElement lblDobEle = webDriver.findElement(By.xpath(lblDobXPath));
+
+        //button[@type]
+        String FORM_XPATH_BTN = "//button[@type ='%s']";
+        String btnSubmitXPath = String.format(FORM_XPATH_BTN, "submit");
+        String btnClearXPath = String.format(FORM_XPATH_BTN, "reset");
+        WebElement btnSubmitEle = webDriver.findElement(By.xpath(btnSubmitXPath));
+        WebElement btnClearEle = webDriver.findElement(By.xpath(btnClearXPath));
+
+        //textarea[@id]
+        String FORM_XPATH_TEXTAREA = "//textarea[@id = '%s']";
+        String lblAddressTxtAraXPath = String.format(FORM_XPATH_TEXTAREA, "address");
+        String lblInfoTxtAreaXPath = String.format(FORM_XPATH_TEXTAREA, "outputTextStudent");
+        WebElement lblAddressEle = webDriver.findElement(By.xpath(lblAddressTxtAraXPath));
+        WebElement lblTxtAreaEle = webDriver.findElement(By.xpath(lblInfoTxtAreaXPath));
+
+
     }
 
     /**
