e3fe1a9b77e3592bed9d52c278b6e03778227405
hoangnhathai <hoangnhathai.95@gmail.com>
2025-05-30 06:00:26 +0700

    Em nộp bài cho Lesson 4 và 5

--- src/test/java/com/testek/study/lesson04/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson04/exercise/SolutionTest.java
@@ -12,7 +12,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.Objects;
+
 import com.testek.study.common.Menu;
+
 import static java.lang.Thread.sleep;
 
 @Getter
@@ -47,7 +49,73 @@ public class SolutionTest {
     public void findLocatorWithCSSOfTextBox() {
         gotoTestWebsite(Menu.TEXT_BOX);
 
+
         // Handle other elements similarly
+        String txtTitleWebCssContainOfTextBox = "h3[test-id *='textbox-form-title']";
+        WebElement txtTitleWebCssContainEle = webDriver.findElement(By.cssSelector(txtTitleWebCssContainOfTextBox));
+
+        String lblNameCssContainOfTextBox = "label[test-id *='textbox-form-label-name']";
+        WebElement lblLabelNameCssContainEle = webDriver.findElement(By.cssSelector(lblNameCssContainOfTextBox));
+
+        String txtLabelNameCssContainOfTextBox = "input[placeholder *='Nhập họ tên']";
+        WebElement txtLabelNameCssContainEle = webDriver.findElement(By.cssSelector(txtLabelNameCssContainOfTextBox));
+
+        String lblEmailCssContainOfTextBox = "label[test-id *='textbox-form-label-email']";
+        WebElement lblEmailCssContainEle = webDriver.findElement(By.cssSelector(lblEmailCssContainOfTextBox));
+
+        String txtEmailCssContainOfTextBox = "input[placeholder *='Nhập email']";
+        WebElement txtEmailCssContainEle = webDriver.findElement(By.cssSelector(txtEmailCssContainOfTextBox));
+
+        String lblPhoneCssContainOfTextBox = "label[test-id *='textbox-form-label-phone']";
+        WebElement lblPhoneCssContainEle = webDriver.findElement(By.cssSelector(lblPhoneCssContainOfTextBox));
+
+        String txtPhoneCssOfTextBox = "input[placeholder *='Nhập số điện thoại']";
+        WebElement txtPhoneCssContainEle = webDriver.findElement(By.cssSelector(txtPhoneCssOfTextBox));
+
+        String btnSubmitCssContainOfTextBox = "button[test-id *='textbox-form-submit']";
+        WebElement btnSubmitCssContainEle = webDriver.findElement(By.cssSelector(btnSubmitCssContainOfTextBox));
+
+        String btnResetCssContainOfTextBox = "button[test-id *='textbox-form-reset']";
+        WebElement btnResetCssContainEle = webDriver.findElement(By.cssSelector(btnResetCssContainOfTextBox));
+
+        String txtOutputTitleContainCssOfTextBox = "h3[test-id *='textbox-form-output-title']";
+        WebElement txtOutputTitleCssContainEle = webDriver.findElement(By.cssSelector(txtOutputTitleContainCssOfTextBox));
+
+        String txtAreaOutputCssContainOfTextBox = "textarea[test-id *='textbox-form-output']";
+        WebElement txtAreaOutputCssContainEle = webDriver.findElement(By.cssSelector(txtAreaOutputCssContainOfTextBox));
+        // Start-with
+        String txtTitleWebCssStartOfTextBox = "h3[test-id^='textbox-form-title']";
+        WebElement txtTitleCssStartEle = webDriver.findElement(By.cssSelector(txtTitleWebCssStartOfTextBox));
+        String lblNameCssStartOfTextBox = "label[test-id ^='textbox-form-label-name']";
+        WebElement lblLabelNameCssStartEle = webDriver.findElement(By.cssSelector(lblNameCssStartOfTextBox));
+
+        String txtLabelNameCssStartOfTextBox = "input[placeholder ^='Nhập họ tên']";
+        WebElement txtLabelNameCssStartEle = webDriver.findElement(By.cssSelector(txtLabelNameCssStartOfTextBox));
+
+        String lblEmailCssStartOfTextBox = "label[test-id ^='textbox-form-label-email']";
+        WebElement lblEmailCssStartEle = webDriver.findElement(By.cssSelector(lblEmailCssStartOfTextBox));
+
+        String txtEmailCssStartOfTextBox = "input[placeholder ^='Nhập email']";
+        WebElement txtEmailCssStartEle = webDriver.findElement(By.cssSelector(txtEmailCssStartOfTextBox));
+
+        String lblPhoneCssStartOfTextBox = "label[test-id ^='textbox-form-label-phone']";
+        WebElement lblPhoneCssStartEle = webDriver.findElement(By.cssSelector(lblPhoneCssStartOfTextBox));
+
+        String txtPhoneCssStartOfTextBox = "input[placeholder ^='Nhập số điện thoại']";
+        WebElement txtPhoneCssStartEle = webDriver.findElement(By.cssSelector(txtPhoneCssStartOfTextBox));
+
+        String btnSubmitCssStartOfTextBox = "button[test-id ^='textbox-form-submit']";
+        WebElement btnSubmitCssStartEle = webDriver.findElement(By.cssSelector(btnSubmitCssStartOfTextBox));
+
+        String btnResetCssStartOfTextBox = "button[test-id ^='textbox-form-reset']";
+        WebElement btnResetCssStartEle = webDriver.findElement(By.cssSelector(btnResetCssStartOfTextBox));
+
+        String txtOutputTitleStartCssOfTextBox = "h3[test-id ^='textbox-form-output-title']";
+        WebElement txtOutputTitleCssStartEle = webDriver.findElement(By.cssSelector(txtOutputTitleStartCssOfTextBox));
+
+        String txtAreaOutputCssStartOfTextBox = "textarea[test-id ^='textbox-form-output']";
+        WebElement txtAreaOutputCssStartEle = webDriver.findElement(By.cssSelector(txtAreaOutputCssStartOfTextBox));
+
 
     }
 
@@ -59,6 +127,22 @@ public class SolutionTest {
         gotoTestWebsite(Menu.TEXT_BOX);
 
         // Handle other elements similarly
+        // Nhập thông tin người dùng
+        String txtTitleWebXpathContainOfTextBox = "//h3[contains(@test-id ,'textbox-form-title')]";
+        WebElement txtTitleXpathContainsEle = webDriver.findElement(By.xpath(txtTitleWebXpathContainOfTextBox));
+        String txtTitleWebXpathStartOfTextBox = "//h3[starts-with(@test-id ,'textbox-form-title')]";
+        WebElement txtTitleWebXpathStartEle = webDriver.findElement(By.xpath(txtTitleWebXpathStartOfTextBox));
+        String txtTitleWebXpathTextOfTextBox = "//h3[normalize-space() = 'Nhập thông tin người dùng:']";
+        WebElement txtTitleWebXpathTextEle = webDriver.findElement(By.xpath(txtTitleWebXpathTextOfTextBox));
+        String txtTitleWebXpathDescendantOfTextBox = "//div[@test-id='textbox-form-container']/descendant::h3[@test-id='textbox-form-title']";
+        WebElement txtTitleWebXpathDescendantEle = webDriver.findElement(By.xpath(txtTitleWebXpathDescendantOfTextBox));
+        String txtTitleWebXpathPrecedingOfTextBox = "//input[@placeholder='Nhập họ tên']/preceding::h3[@test-id='textbox-form-title']";
+        WebElement txtTitleWebXpathPrecedingEle = webDriver.findElement(By.xpath(txtTitleWebXpathPrecedingOfTextBox));
+        String txtTitleWebXpathFollowingOfTextBox = "//h4[@test-id='about-me-separator']/following::h3[@test-id='textbox-form-title']";
+        WebElement txtTitleWebXpathFollowingEle = webDriver.findElement(By.xpath(txtTitleWebXpathFollowingOfTextBox));
+        String txtTitleWebXpathChildOfTextBox = "//div[@test-id='textbox-form-container']/child::h3[@test-id='textbox-form-title']";
+        WebElement txtTitleWebXpathChildEle = webDriver.findElement(By.xpath(txtTitleWebXpathChildOfTextBox));
+
 
     }
 
@@ -70,6 +154,36 @@ public class SolutionTest {
         gotoTestWebsite(Menu.RADIO_BUTTON);
 
         // Handle other elements similarly
+        String txtRadioTitleCssContainOfRadioButton = "h3[test-id *='radio-title']";
+        WebElement txtRadioTileCssContainEle = webDriver.findElement(By.cssSelector(txtRadioTitleCssContainOfRadioButton));
+        String btnYesCssContainOfRadioButton = "input[test-id *='radio-input-yes']";
+        WebElement btnYesCssContainEle = webDriver.findElement(By.cssSelector(btnYesCssContainOfRadioButton));
+        String lblYesCssContainOfRadioButton = "label[test-id *='radio-label-yes']";
+        WebElement lblYesCssContainEle = webDriver.findElement(By.cssSelector(lblYesCssContainOfRadioButton));
+        String btnImpressiveCssContainOfRadioButton = "input[test-id *='radio-input-impressive']";
+        WebElement btnImpressiveSiteCssContainEle = webDriver.findElement(By.cssSelector(btnImpressiveCssContainOfRadioButton));
+        String lblImpressiveCssContainOfRadioButton = "label[test-id *='radio-label-impressive']";
+        WebElement lblImpressiveCssContainEle = webDriver.findElement(By.cssSelector(lblImpressiveCssContainOfRadioButton));
+        String btnNoCssContainOfRadioButton = "input[test-id *='radio-input-no']";
+        WebElement btnNoSiteCssContainEle = webDriver.findElement(By.cssSelector(btnNoCssContainOfRadioButton));
+        String lblNoCssContainOfRadioButton = "label[test-id *='radio-label-no']";
+        WebElement lblNoCssContainEle = webDriver.findElement(By.cssSelector(lblNoCssContainOfRadioButton));
+
+        //Starts-with
+        String txtRadioTitleCssStartsWithRadioButton = "h3[test-id ^='radio-title']";
+        WebElement txtRadioTileCssStartsWithEle = webDriver.findElement(By.cssSelector(txtRadioTitleCssStartsWithRadioButton));
+        String btnYesCssStartsWithRadioButton = "input[test-id ^='radio-input-yes']";
+        WebElement btnYesCssStartsWithEle = webDriver.findElement(By.cssSelector(btnYesCssStartsWithRadioButton));
+        String lblYesCssStartsWithRadioButton = "label[test-id ^='radio-label-yes']";
+        WebElement lblYesCssStartsWithEle = webDriver.findElement(By.cssSelector(lblYesCssStartsWithRadioButton));
+        String btnImpressiveCssStartsWithRadioButton = "input[test-id ^='radio-input-impressive']";
+        WebElement btnImpressiveSiteCssStartsWithEle = webDriver.findElement(By.cssSelector(btnImpressiveCssStartsWithRadioButton));
+        String lblImpressiveCssStartsWithRadioButton = "label[test-id ^='radio-label-impressive']";
+        WebElement lblImpressiveCssStartsWithEle = webDriver.findElement(By.cssSelector(lblImpressiveCssStartsWithRadioButton));
+        String btnNoCssStartsWithRadioButton = "input[test-id ^='radio-input-no']";
+        WebElement btnNoSiteCssStartsWithEle = webDriver.findElement(By.cssSelector(btnNoCssStartsWithRadioButton));
+        String lblNoCssStartsWithRadioButton = "label[test-id ^='radio-label-no']";
+        WebElement lblNoCssStartsWithEle = webDriver.findElement(By.cssSelector(lblNoCssStartsWithRadioButton));
 
     }
 
@@ -81,6 +195,22 @@ public class SolutionTest {
         gotoTestWebsite(Menu.RADIO_BUTTON);
 
         // Handle other elements similarly
+        //Do you like the site?
+        String txtRadioTitleXpathContainOfRadioButton = "//h3[contains(@test-id ,'radio-title')]";
+        WebElement txtRadioTitleXpathContainsEle = webDriver.findElement(By.xpath(txtRadioTitleXpathContainOfRadioButton));
+        String txtRadioTitleXpathStartOfRadioButton = "//h3[starts-with(@test-id ,'radio-title')]";
+        WebElement txtRadioTitleXpathStartEle = webDriver.findElement(By.xpath(txtRadioTitleXpathStartOfRadioButton));
+        String txtRadioTitleXpathTextOfRadioButton = "//h3[normalize-space() = 'Do you like the site?']";
+        WebElement txtRadioTitleXpathTextEle = webDriver.findElement(By.xpath(txtRadioTitleXpathTextOfRadioButton));
+        String txtRadioTitleXpathDescendantOfRadioButton = "//div[@test-id='radio-section']/descendant::h3[@test-id='radio-title']";
+        WebElement txtTitleWebXpathDescendantEle = webDriver.findElement(By.xpath(txtRadioTitleXpathDescendantOfRadioButton));
+        String txtRadioTitleXpathPrecedingOfRadioButton = "//input[@test-id='radio-input-yes']/preceding::h3[@test-id='radio-title']";
+        WebElement txtRadioTitleXpathPrecedingEle = webDriver.findElement(By.xpath(txtRadioTitleXpathPrecedingOfRadioButton));
+        String txtRadioTitleXpathFollowingOfRadioButton = "//div[@test-id='checkbox-section']/following::h3[@test-id='radio-title']";
+        WebElement txtRadioTitleXpathFollowingEle = webDriver.findElement(By.xpath(txtRadioTitleXpathFollowingOfRadioButton));
+        String txtRadioTitleXpathChildOfRadioButton = "//div[@test-id='radio-container']/child::h3[@test-id='radio-title']";
+        WebElement txtRadioTitleXpathChildEle = webDriver.findElement(By.xpath(txtRadioTitleXpathChildOfRadioButton));
+
 
     }
 
@@ -129,7 +259,6 @@ public class SolutionTest {
         // Handle other elements similarly
 
     }
-
 
 
     /**
--- src/test/java/com/testek/study/lesson05/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson05/exercise/SolutionTest.java
@@ -47,6 +47,92 @@ public class SolutionTest {
         gotoTestWebsite();
 
         // Handle other elements similarly
+        String FORM_CSS_H3_TEXT = "h3[test-id*='%s']";
+        String FORM_CSS_LABEL_TEXT = "label[test-id*='%s']";
+        String FORM_CSS_INPUT_TEXT = "input[test-id*='%s']";
+        String FORM_CSS_MARK_BUTTON = "input[test-id*='%s']";
+        String FORM_CSS_INPUT_TXTAREA = "textarea[test-id*='%s']";
+        String FORM_CSS_INPUT_BUTTON = "button[test-id*='%s']";
+
+        String txtFormTitleCSS = String.format(FORM_CSS_H3_TEXT, "browser-windows-title");
+        String txtStudentRegistrationCSS = String.format(FORM_CSS_H3_TEXT, "student-registration-title");
+        String lblFirstNameCSS = String.format(FORM_CSS_LABEL_TEXT, "label-firstName");
+        String txtFirstNameCSS = String.format(FORM_CSS_INPUT_TEXT, "input-firstName");
+        String lblLastNameCSS = String.format(FORM_CSS_LABEL_TEXT, "label-lastName");
+        String txtLastNameCSS = String.format(FORM_CSS_INPUT_TEXT, "input-lastName");
+        String lblEmailCSS = String.format(FORM_CSS_LABEL_TEXT, "label-email");
+        String txtEmailCSS = String.format(FORM_CSS_INPUT_TEXT, "input-email");
+        String lblGenderCSS = String.format(FORM_CSS_LABEL_TEXT, "label-gender");
+        String btnMaleCSS = String.format(FORM_CSS_MARK_BUTTON, "input-male");
+        String btnFemaleCSS = String.format(FORM_CSS_MARK_BUTTON, "input-female");
+        String lblMaleCSS = String.format(FORM_CSS_LABEL_TEXT, "label-male");
+        String lblFemaleCSS = String.format(FORM_CSS_LABEL_TEXT, "label-female");
+        String lblMobileCSS = String.format(FORM_CSS_LABEL_TEXT, "label-mobile");
+        String txtMobileCSS = String.format(FORM_CSS_INPUT_TEXT, "input-mobile");
+        String lblDobCSS = String.format(FORM_CSS_LABEL_TEXT, "label-dob");
+        String txtDobCSS = String.format(FORM_CSS_INPUT_TEXT, "input-dob");
+        String lblSubjectCSS = String.format(FORM_CSS_LABEL_TEXT, "label-subjects");
+        String txtSubjectCSS = String.format(FORM_CSS_INPUT_TEXT, "input-subjects");
+        String lblHobbyCSS = String.format(FORM_CSS_LABEL_TEXT, "label-hobbies");
+        String btnReadingCSS = String.format(FORM_CSS_MARK_BUTTON, "input-reading");
+        String lblReadingCSS = String.format(FORM_CSS_LABEL_TEXT, "label-reading");
+        String btnSportsCSS = String.format(FORM_CSS_MARK_BUTTON, "input-sports");
+        String lblSportsCSS = String.format(FORM_CSS_LABEL_TEXT, "label-sports");
+        String btnMusicCSS = String.format(FORM_CSS_MARK_BUTTON, "input-music");
+        String lblMusicCSS = String.format(FORM_CSS_LABEL_TEXT, "label-music");
+        String lblAddressCSS = String.format(FORM_CSS_LABEL_TEXT, "label-address");
+        String txtAreaAddressCSS = String.format(FORM_CSS_INPUT_TXTAREA, "textarea-address");
+        String btnSubmitCSS = String.format(FORM_CSS_INPUT_BUTTON, "submit-button");
+        String btnClearCSS = String.format(FORM_CSS_INPUT_BUTTON, "reset-button");
+        String txtSubmittedTitleCSS = String.format(FORM_CSS_H3_TEXT, "submitted-info-title");
+        String txtOutputTextStudentCSS = String.format(FORM_CSS_INPUT_TXTAREA, "outputTextStudent");
+
+        //Find web element
+        webDriver.findElement(By.cssSelector(txtFormTitleCSS));
+        webDriver.findElement(By.cssSelector(txtStudentRegistrationCSS));
+        webDriver.findElement(By.cssSelector(lblFirstNameCSS));
+        webDriver.findElement(By.cssSelector(txtFirstNameCSS));
+        webDriver.findElement(By.cssSelector(lblLastNameCSS));
+        webDriver.findElement(By.cssSelector(txtLastNameCSS));
+        webDriver.findElement(By.cssSelector(lblEmailCSS));
+        webDriver.findElement(By.cssSelector(txtEmailCSS));
+        webDriver.findElement(By.cssSelector(lblGenderCSS));
+        webDriver.findElement(By.cssSelector(btnMaleCSS));
+        webDriver.findElement(By.cssSelector(btnFemaleCSS));
+        webDriver.findElement(By.cssSelector(lblFemaleCSS));
+        webDriver.findElement(By.cssSelector(lblMaleCSS));
+        webDriver.findElement(By.cssSelector(lblDobCSS));
+        webDriver.findElement(By.cssSelector(lblMobileCSS));
+        webDriver.findElement(By.cssSelector(txtMobileCSS));
+        webDriver.findElement(By.cssSelector(txtDobCSS));
+        webDriver.findElement(By.cssSelector(lblSubjectCSS));
+        webDriver.findElement(By.cssSelector(txtSubjectCSS));
+        webDriver.findElement(By.cssSelector(lblHobbyCSS));
+        webDriver.findElement(By.cssSelector(btnReadingCSS));
+        webDriver.findElement(By.cssSelector(lblReadingCSS));
+        webDriver.findElement(By.cssSelector(btnSportsCSS));
+        webDriver.findElement(By.cssSelector(lblSportsCSS));
+        webDriver.findElement(By.cssSelector(btnMusicCSS));
+        webDriver.findElement(By.cssSelector(lblMusicCSS));
+        webDriver.findElement(By.cssSelector(lblAddressCSS));
+        webDriver.findElement(By.cssSelector(txtAreaAddressCSS));
+        webDriver.findElement(By.cssSelector(btnSubmitCSS));
+        webDriver.findElement(By.cssSelector(btnClearCSS));
+        webDriver.findElement(By.cssSelector(txtSubmittedTitleCSS));
+        webDriver.findElement(By.cssSelector(txtOutputTextStudentCSS));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
@@ -55,6 +141,82 @@ public class SolutionTest {
         gotoTestWebsite();
 
         // Handle other elements similarly
+        String FORM_XPATH_H3_TEXT = "//h3[contains(@test-id, '%s')]";
+        String FORM_XPATH_LABEL_TEXT = "//label[contains(@test-id, '%s')]";
+        String FORM_XPATH_INPUT_TEXT = "//input[contains(@test-id, '%s')]";
+        String FORM_XPATH_MARK_BUTTON = "//input[contains(@test-id, '%s')]";
+        String FORM_XPATH_INPUT_TXTAREA = "//textarea[contains(@test-id, '%s')]";
+        String FORM_XPATH_INPUT_BUTTON = "//button[contains(@test-id, '%s')]";
+
+        String txtFormTitleXpath = String.format(FORM_XPATH_H3_TEXT, "browser-windows-title");
+        String txtStudentRegistrationXpath = String.format(FORM_XPATH_H3_TEXT, "student-registration-title");
+        String lblFirstNameXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-firstName");
+        String txtFirstNameXpath = String.format(FORM_XPATH_INPUT_TEXT, "input-firstName");
+        String lblLastNameXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-lastName");
+        String txtLastNameXpath = String.format(FORM_XPATH_INPUT_TEXT, "input-lastName");
+        String lblEmailXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-email");
+        String txtEmailXpath = String.format(FORM_XPATH_INPUT_TEXT, "input-email");
+        String lblGenderXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-gender");
+        String btnMaleXpath = String.format(FORM_XPATH_MARK_BUTTON, "input-male");
+        String btnFemaleXpath = String.format(FORM_XPATH_MARK_BUTTON, "input-female");
+        String lblMaleXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-male");
+        String lblFemaleXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-female");
+        String lblMobileXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-mobile");
+        String txtMobileXpath = String.format(FORM_XPATH_INPUT_TEXT, "input-mobile");
+        String lblDobXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-dob");
+        String txtDobXpath = String.format(FORM_XPATH_INPUT_TEXT, "input-dob");
+        String lblSubjectXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-subjects");
+        String txtSubjectXpath = String.format(FORM_XPATH_INPUT_TEXT, "input-subjects");
+        String lblHobbyXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-hobbies");
+        String btnReadingXpath = String.format(FORM_XPATH_MARK_BUTTON, "input-reading");
+        String lblReadingXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-reading");
+        String btnSportsXpath = String.format(FORM_XPATH_MARK_BUTTON, "input-sports");
+        String lblSportsXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-sports");
+        String btnMusicXpath = String.format(FORM_XPATH_MARK_BUTTON, "input-music");
+        String lblMusicXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-music");
+        String lblAddressXpath = String.format(FORM_XPATH_LABEL_TEXT, "label-address");
+        String txtAreaAddressXpath = String.format(FORM_XPATH_INPUT_TXTAREA, "textarea-address");
+        String btnSubmitXpath = String.format(FORM_XPATH_INPUT_BUTTON, "submit-button");
+        String btnClearXpath = String.format(FORM_XPATH_INPUT_BUTTON, "reset-button");
+        String txtSubmittedTitleXpath = String.format(FORM_XPATH_H3_TEXT, "submitted-info-title");
+        String txtOutputTextStudentXpath = String.format(FORM_XPATH_INPUT_TXTAREA, "outputTextStudent");
+
+        //Find web element
+        webDriver.findElement(By.xpath(txtFormTitleXpath));
+        webDriver.findElement(By.xpath(txtStudentRegistrationXpath));
+        webDriver.findElement(By.xpath(lblFirstNameXpath));
+        webDriver.findElement(By.xpath(txtFirstNameXpath));
+        webDriver.findElement(By.xpath(lblLastNameXpath));
+        webDriver.findElement(By.xpath(txtLastNameXpath));
+        webDriver.findElement(By.xpath(lblEmailXpath));
+        webDriver.findElement(By.xpath(txtEmailXpath));
+        webDriver.findElement(By.xpath(lblGenderXpath));
+        webDriver.findElement(By.xpath(btnMaleXpath));
+        webDriver.findElement(By.xpath(btnFemaleXpath));
+        webDriver.findElement(By.xpath(lblMaleXpath));
+        webDriver.findElement(By.xpath(lblFemaleXpath));
+        webDriver.findElement(By.xpath(lblMobileXpath));
+        webDriver.findElement(By.xpath(txtMobileXpath));
+        webDriver.findElement(By.xpath(lblDobXpath));
+        webDriver.findElement(By.xpath(txtDobXpath));
+        webDriver.findElement(By.xpath(lblSubjectXpath));
+        webDriver.findElement(By.xpath(txtSubjectXpath));
+        webDriver.findElement(By.xpath(lblHobbyXpath));
+        webDriver.findElement(By.xpath(btnReadingXpath));
+        webDriver.findElement(By.xpath(lblReadingXpath));
+        webDriver.findElement(By.xpath(btnSportsXpath));
+        webDriver.findElement(By.xpath(lblSportsXpath));
+        webDriver.findElement(By.xpath(btnMusicXpath));
+        webDriver.findElement(By.xpath(lblMusicXpath));
+        webDriver.findElement(By.xpath(lblAddressXpath));
+        webDriver.findElement(By.xpath(txtAreaAddressXpath));
+        webDriver.findElement(By.xpath(btnSubmitXpath));
+        webDriver.findElement(By.xpath(btnClearXpath));
+        webDriver.findElement(By.xpath(txtSubmittedTitleXpath));
+        webDriver.findElement(By.xpath(txtOutputTextStudentXpath));
+
+
+
 
     }
 
@@ -65,6 +227,8 @@ public class SolutionTest {
     @Test(description = "Dynamic Locator Test")
     public void testDynamicLocator() {
         gotoTestWebsite();
+
+
 
         // Define the locator format then apply to elements
     }
@@ -85,17 +249,15 @@ public class SolutionTest {
         WebElement mnuEle = webDriver.findElement(By.xpath(String.format(XPATH_MENU_FORMAT, Menu.STUDENT_FORM.getName())));
         mnuEle.click();
 
-        waitForDebug(5000);
+        waitForDebug();
     }
 
     /**
      * Sleep for debugging
-     *
-     * @param milliseconds : number of milliseconds
      */
-    private void waitForDebug(long milliseconds) {
+    private void waitForDebug() {
         try {
-            sleep(milliseconds);
+            sleep(5000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
