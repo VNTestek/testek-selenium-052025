351414c6d21bf24aed7bda040cebb7b5754cba75
Nguyen Ngoc Anh <nngocanh8896@gmail.com>
2025-06-02 23:28:18 +0700

    Nguyen Ngoc Anh - Demo + Homework Lesson06

--- src/test/java/com/testek/study/lesson06/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson06/exercise/SolutionTest.java
@@ -27,6 +27,8 @@ public class SolutionTest {
      */
     @BeforeClass
     public void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--max-window-size");
         chromeOptions.addArguments("--remote-allow-origins=*");
@@ -49,7 +51,78 @@ public class SolutionTest {
     public void testInteraction() {
         gotoTestWebsite();
 
+        // Declare Dynamic XPathAdd commentMore actions
+        String FORM_XPATH_LABEL_TEXT = "//label[@test-id='%s']";
+        String FORM_XPATH_INPUT_TEXT = "//input[@test-id = '%s']";
+        String FORM_XPATH_TEXTAREA_INPUT = "//textarea[@id='%s']";
+        String FORM_XPATH_BUTTON_TEXT = "//button[@id='%s']";
+
+        // Interact with element
+        // 1. Input First Name
+        String txtFistNameXPath = String.format(FORM_XPATH_INPUT_TEXT, "input-firstName");
+        WebElement txtFistNameEle = webDriver.findElement(By.xpath(txtFistNameXPath));
+        txtFistNameEle.clear();
+        txtFistNameEle.sendKeys("Ngoc Anh");
+
+        // 2. Input Last Name
+        String txtLastNameXPath = String.format(FORM_XPATH_INPUT_TEXT, "input-lastName");
+        WebElement txtLastNameEle = webDriver.findElement((By.xpath(txtLastNameXPath)));
+        txtLastNameEle.clear();
+        txtLastNameEle.sendKeys("Nguyen");
+
+        // 3. Input Email
+        String txtEmailXPath = String.format(FORM_XPATH_INPUT_TEXT, "input-email");
+        WebElement txtEmailEle = webDriver.findElement((By.xpath(txtEmailXPath)));
+        txtEmailEle.clear();
+        txtEmailEle.sendKeys("ngocanh@gmail.com");
+
+        // 4. Select gender is male
+        String rdoMaleXPath = String.format(FORM_XPATH_LABEL_TEXT, "label-male");
+        WebElement rdoMaleEle = webDriver.findElement((By.xpath(rdoMaleXPath)));
+        rdoMaleEle.click();
+
+        // 5. Input Phone
+        String txtMobileXPath = String.format(FORM_XPATH_INPUT_TEXT, "input-mobile");
+        WebElement txtMobileEle = webDriver.findElement((By.xpath(txtMobileXPath)));
+        txtMobileEle.clear();
+        txtMobileEle.sendKeys("0931101896");
+
+        // 6. Select date
+        String datDobXPath = String.format(FORM_XPATH_INPUT_TEXT, "input-dob");
+        WebElement datDobEle = webDriver.findElement((By.xpath(datDobXPath)));
+        datDobEle.click();
+        datDobEle.sendKeys("06/02/2025");
+
+        // 7. Input Subject
+        String txtSubjectsXPath = String.format(FORM_XPATH_INPUT_TEXT, "input-subjects");
+        WebElement txtSubjectsEle = webDriver.findElement((By.xpath(txtSubjectsXPath)));
+        txtSubjectsEle.clear();
+        txtSubjectsEle.sendKeys("test");
+
+        // 8. Select hobbies
+        String chkRedingXPath = String.format(FORM_XPATH_LABEL_TEXT, "label-reading");
+        String chkSportsXPath = String.format(FORM_XPATH_LABEL_TEXT, "label-sports");
+        String chkMusicXPath = String.format(FORM_XPATH_LABEL_TEXT, "label-music");
+        WebElement chkRedingXEle = webDriver.findElement((By.xpath(chkRedingXPath)));
+        WebElement chkSportsEle = webDriver.findElement((By.xpath(chkSportsXPath)));
+        WebElement chkMusicEle = webDriver.findElement((By.xpath(chkMusicXPath)));
+        chkRedingXEle.click();
+        chkSportsEle.click();
+        chkMusicEle.click();
+
+        //9. Input Address
+        String txtAddressXPath = String.format(FORM_XPATH_TEXTAREA_INPUT, "address");
+        WebElement txtAddressEle = webDriver.findElement((By.xpath(txtAddressXPath)));
+        txtAddressEle.clear();
+        txtAddressEle.sendKeys("Ha Noi");
+
+        // 10. Submit
+        String bntSubmitXPath = String.format(FORM_XPATH_BUTTON_TEXT, "submit-button");
+        WebElement bntSubmitEle = webDriver.findElement((By.xpath(bntSubmitXPath)));
+        bntSubmitEle.click();
+
         // Handle your code here
+
     }
 
     /**
