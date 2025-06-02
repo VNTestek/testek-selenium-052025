674f810319adf36f0b2cb9e2bcf26bcb41bfab9c
base project <danghuong22129@gmail.com>
2025-06-03 00:24:35 +0700

    [DangHuong_Lesson6] Find Locator with Xpath And Interact Element

--- src/test/java/com/testek/study/lesson06/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson06/exercise/SolutionTest.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.Objects;
 
 import static java.lang.Thread.sleep;
@@ -48,8 +51,41 @@ public class SolutionTest {
     @Test(description = "Element Interaction")
     public void testInteraction() {
         gotoTestWebsite();
-
         // Handle your code here
+
+        // button
+        String FORM_BUTTON_XPATH = "//button[@test-id='%s']";
+        String btnClickMeXpath = "button-click-me";
+        String btnConfirmMeXpath = "button-confirm-me";
+        String btnPromptXpath = "button-prompt-me";
+        String btnOneXpath = "button1";
+        String btnTwiceXpath = "button2";
+        String btnThirdXpath = "button3";
+
+        WebElement btnClickMeEle = webDriver.findElement(By.xpath(String.format(FORM_BUTTON_XPATH, btnClickMeXpath)));
+        WebElement btnConfirmMeEle = webDriver.findElement(By.xpath(String.format(FORM_BUTTON_XPATH, btnConfirmMeXpath)));
+        WebElement btnPromptEle = webDriver.findElement(By.xpath(String.format(FORM_BUTTON_XPATH, btnPromptXpath)));
+        WebElement btnOneEle = webDriver.findElement(By.xpath(String.format(FORM_BUTTON_XPATH, btnOneXpath)));
+        WebElement btnTwiceEle = webDriver.findElement(By.xpath(String.format(FORM_BUTTON_XPATH, btnTwiceXpath)));
+        WebElement btnThirdEle = webDriver.findElement(By.xpath(String.format(FORM_BUTTON_XPATH, btnThirdXpath)));
+
+
+
+        waitForDebug(5000);
+
+        // chua fix duoc loi khong click dc
+        btnOneEle.click();
+
+        // tim thu cach khac nhung cung khong dc
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
+        WebElement btnClickMeEle2 = wait.until(ExpectedConditions.elementToBeClickable(btnOneEle));
+        btnClickMeEle2.click();
+
+
+        String txtValueXpath = "//div[@test-id='buttons-interaction-result']/child::p";
+        WebElement txtValueEle = webDriver.findElement(By.xpath(txtValueXpath));
+        System.out.println(txtValueEle.getAttribute("textContent"));
+
     }
 
     /**
@@ -69,6 +105,66 @@ public class SolutionTest {
         mnuEle.click();
 
         waitForDebug(5000);
+    }
+
+
+    @Test
+    private void findElementWithXpath() {
+        gotoTestWebsite();
+
+        // input
+        String FORM_INPUT_XPATH = "//input[@test-id='%s']";
+        String inputFirstNameXpath = "input-firstName";
+        String inputLastNameXpath = "input-lastName";
+        String inputEmailXpath = "input-email";
+        String inputMobileXpath = "input-mobile";
+        String inputSubjectsXpath = "input-subjects";
+        String inputDobXpath = "input-dob";
+        WebElement inputFirstNameEle = webDriver.findElement(By.xpath(String.format(FORM_INPUT_XPATH, inputFirstNameXpath)));
+        WebElement inputLastNameEle = webDriver.findElement(By.xpath(String.format(FORM_INPUT_XPATH, inputLastNameXpath)));
+        WebElement inputEmailEle = webDriver.findElement(By.xpath(String.format(FORM_INPUT_XPATH, inputEmailXpath)));
+        WebElement inputMobileEle = webDriver.findElement(By.xpath(String.format(FORM_INPUT_XPATH, inputMobileXpath)));
+        WebElement inputSubjectsEle = webDriver.findElement(By.xpath(String.format(FORM_INPUT_XPATH, inputSubjectsXpath)));
+        WebElement inputDobEle = webDriver.findElement(By.xpath(String.format(FORM_INPUT_XPATH, inputDobXpath)));
+
+        // label radio
+        String FORM_RDO_LBL_XPATH = "//label[@test-id='%s']";
+        String lblRdoMaleXpath = "label-male";
+        String lblRdoFeMaleXpath = "label-female";
+        WebElement lblRdoMaleEle = webDriver.findElement(By.xpath(String.format(FORM_RDO_LBL_XPATH, lblRdoMaleXpath)));
+        WebElement lblRdoFeMaleEle = webDriver.findElement(By.xpath(String.format(FORM_RDO_LBL_XPATH, lblRdoFeMaleXpath)));
+
+        // radio
+        String FORM_RDO_XPATH = "//label[@test-id='%s']/child::input";
+        WebElement rdoMaleEle = webDriver.findElement(By.xpath(String.format(FORM_RDO_XPATH, lblRdoMaleXpath)));
+        WebElement rdoFeMaleEle = webDriver.findElement(By.xpath(String.format(FORM_RDO_XPATH, lblRdoFeMaleXpath)));
+
+        // label checkbox
+        String FORM_LAB_XPATH = "//label[@test-id='%s']";
+        String lblReadingXpath = "label-reading";
+        String lblSportsXpath = "label-sports";
+        String lblMusicXpath = "label-music";
+        WebElement lblReadingEle = webDriver.findElement(By.xpath(String.format(FORM_LAB_XPATH, lblReadingXpath)));
+        WebElement lblSportsEle = webDriver.findElement(By.xpath(String.format(FORM_LAB_XPATH, lblSportsXpath)));
+        WebElement lblMusicEle = webDriver.findElement(By.xpath(String.format(FORM_LAB_XPATH, lblMusicXpath)));
+
+        // checkbox
+        String FORM_CB_XPATH = "//label[@test-id='%s']/child::input";
+        WebElement cbReadingEle = webDriver.findElement(By.xpath(String.format(FORM_CB_XPATH, lblReadingXpath)));
+        WebElement cbSportsEle = webDriver.findElement(By.xpath(String.format(FORM_CB_XPATH, lblSportsXpath)));
+        WebElement cbMusicEle = webDriver.findElement(By.xpath(String.format(FORM_CB_XPATH, lblMusicXpath)));
+
+        // button
+        String FORM_BUTTON_XPATH = "//button[@test-id='%s']";
+        String btnSubmitXpath = "submit-button";
+        String btnResetXpath = "reset-button";
+        WebElement btnSubmitEle = webDriver.findElement(By.xpath(String.format(FORM_BUTTON_XPATH, btnSubmitXpath)));
+        WebElement btnResetEle = webDriver.findElement(By.xpath(String.format(FORM_BUTTON_XPATH, btnResetXpath)));
+
+        // textArea
+        String txtInfoXpath = "//textarea[@test-id='outputTextStudent']";
+        WebElement txtInfoEle = webDriver.findElement(By.xpath(txtInfoXpath));
+
     }
 
     /**
