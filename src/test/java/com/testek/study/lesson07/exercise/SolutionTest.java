e75539e6a9b5cc1c3ce0bebb84c2c091d7a7fa12
anhthai07 <thaithoa307273@gmail.com>
2025-06-05 23:56:39 +0700

    AnhThai - Commit BTVN Lesson07

--- src/test/java/com/testek/study/lesson07/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson07/exercise/SolutionTest.java
@@ -3,9 +3,8 @@ package com.testek.study.lesson07.exercise;
 import com.testek.study.common.Menu;
 import lombok.Getter;
 import lombok.Setter;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
@@ -18,6 +17,7 @@ import static java.lang.Thread.sleep;
 
 @Getter
 @Setter
+@Slf4j
 public class SolutionTest {
     // The driver for interacting with the webpage
     private WebDriver mWebDriver;
@@ -46,25 +46,88 @@ public class SolutionTest {
      * Access to Elements navigation
      */
     @Test(description = "Element Interaction: Student Form")
-    public void testStudentForm() {
-        gotoTestWebsite(Menu.FORM, Menu.STUDENT_FORM);
-
+    public void testPracticeWindows() {
         // Handle your code here
+        String dantriURL = "https://dantri.com.vn/";
+        String testekURL = "https://testek.vn/lab/auto/web-elements/";
 
+        // Get current windows
+        mWebDriver.get(dantriURL);
+        String currentWin = mWebDriver.getWindowHandle();
+        System.out.println(currentWin);
+
+        //NEW TAB
+        WebDriver newTab = mWebDriver.switchTo().newWindow(WindowType.TAB);
+        newTab.get(testekURL);
+        gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.WINDOWS);
+
+        // Back to the first window
+        mWebDriver.switchTo().window(currentWin);
+        waitForDebug(2000);
+
+        //NEW WINDOW
+        WebDriver newWindow = mWebDriver.switchTo().newWindow(WindowType.WINDOW);
+        newWindow.get(testekURL);
+        gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.ALERT);
     }
 
     @Test(description = "Element Interaction: Small Frame")
     public void testSmallFrame() {
         gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.FRAME);
-
         // Handle your code here
+        String SMALL_FRAME = "//iframe[@id = '%s']";
+
+        //confirmAlert
+        String frmSmallXPath = String.format(SMALL_FRAME, "small-frame");
+        WebElement frmSmallEle = mWebDriver.findElement(By.xpath(frmSmallXPath));
+
+        mWebDriver.switchTo().frame(frmSmallEle);
+
+        String smallFrmTitle = "//h1[normalize-space()='This is a Small Frame']";
+        WebElement smallFrmTitleEle = mWebDriver.findElement(By.xpath(smallFrmTitle));
+        System.out.println(smallFrmTitleEle.getText());
+
+        String smallFrmDescription= "//p[normalize-space()='Smaller content can be displayed here.']";
+        WebElement smallFrmDescriptionEle = mWebDriver.findElement(By.xpath(smallFrmDescription));
+        System.out.println(smallFrmDescriptionEle);
+
+        // Back to page contents
+        mWebDriver.switchTo().defaultContent();
+        log.info("Back to the main page");
+        String lblMainPageXpath = "//h1[@id='frames-title']";
+        WebElement lblMainPageEle = mWebDriver.findElement(By.xpath(lblMainPageXpath));
+        String lblMainPage = lblMainPageEle.getAttribute("textContent");
+        System.out.println("Main page title: " + lblMainPage);
+
     }
 
     @Test(description = "Element Interaction: Alert")
     public void testAlert() {
         gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.ALERT);
-
         // Handle your code here
+        String XPATH_BUTTON = "//button[@id = '%s']";
+
+        //confirmAlert
+        String btnConfirmAlertXPath = String.format(XPATH_BUTTON, "confirmAlert");
+        WebElement btnConfirmAlertEle = mWebDriver.findElement(By.xpath(btnConfirmAlertXPath));
+        System.out.println(btnConfirmAlertEle.getText());
+        btnConfirmAlertEle.click();
+        waitForDebug(1000);
+        Alert alertConfirm = mWebDriver.switchTo().alert();
+        System.out.println("ConfirmAlert: " + alertConfirm.getText());
+        alertConfirm.accept();
+
+        //promptAlert
+        String btnPromptAlertXPath = String.format(XPATH_BUTTON, "promptAlert");
+        WebElement btnPromptAlertEle = mWebDriver.findElement(By.xpath(btnPromptAlertXPath));
+        System.out.println(btnPromptAlertEle.getText());
+        btnPromptAlertEle.click();
+        waitForDebug(2000);
+        Alert alertPrompt = mWebDriver.switchTo().alert();
+        System.out.println("PromptAlert: " + alertPrompt.getText());
+        alertPrompt.sendKeys("anhTraiSayHi");
+        alertPrompt.accept();
+
     }
 
      /**
