e7852cd7f662c38766e9c76db98cc2976b180648
vietdung385 <nguyendung5112004@gmail.com>
2025-06-06 00:54:48 +0700

    Em nop Btvn a.

--- src/test/java/com/testek/study/lesson07/exercise/SolutionTest07.java
+++ src/test/java/com/testek/study/lesson07/exercise/SolutionTest07.java
@@ -3,9 +3,7 @@ package com.testek.study.lesson07.exercise;
 import com.testek.study.common.Menu;
 import lombok.Getter;
 import lombok.Setter;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
@@ -18,7 +16,7 @@ import static java.lang.Thread.sleep;
 
 @Getter
 @Setter
-public class SolutionTest {
+public class SolutionTest07 {
     // The driver for interacting with the webpage
     private WebDriver mWebDriver;
 
@@ -47,17 +45,25 @@ public class SolutionTest {
      */
     @Test(description = "Element Interaction: Student Form")
     public void testStudentForm() {
-        gotoTestWebsite(Menu.FORM, Menu.STUDENT_FORM);
-
+        gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.WINDOWS);
         // Handle your code here
+
 
     }
 
     @Test(description = "Element Interaction: Small Frame")
     public void testSmallFrame() {
         gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.FRAME);
-
         // Handle your code here
+        String smallFrameId = "small-frame";
+        WebElement smllFrameEle = mWebDriver.findElement(By.id(smallFrameId));
+        mWebDriver.switchTo().frame(smllFrameEle);
+        String titleSmllFrame = "//h1[text()='This is a Small Frame']";
+        WebElement titleSmllFrameEle = mWebDriver.findElement(By.xpath(titleSmllFrame));
+        System.out.println("Title small frame: " + titleSmllFrameEle.getText());
+        WebElement lblTextEle = mWebDriver.findElement(By.tagName("p"));
+        String txtValue = lblTextEle.getAttribute("Text");
+        System.out.println("Value " + txtValue);
     }
 
     @Test(description = "Element Interaction: Alert")
@@ -65,9 +71,38 @@ public class SolutionTest {
         gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.ALERT);
 
         // Handle your code here
+        String simpAlert = "simpleAlert";
+        WebElement simpAlertEle = mWebDriver.findElement(By.id(simpAlert));
+        simpAlertEle.click();
+        Alert simpleAl = mWebDriver.switchTo().alert();
+        System.out.println("Text: " + simpleAl.getText());
+        simpleAl.accept();
+
+        String confAlert = "confirmAlert";
+        WebElement confAlertEle = mWebDriver.findElement(By.id(confAlert));
+        confAlertEle.click();
+        Alert confAl = mWebDriver.switchTo().alert();
+        System.out.println("Text: " + confAl.getText());
+        confAl.accept();
+
+        String promptAlert = "promptAlert";
+        WebElement promAlertEle = mWebDriver.findElement(By.id(promptAlert));
+        promAlertEle.click();
+        Alert promfAl = mWebDriver.switchTo().alert();
+        System.out.println("Text: " + promfAl.getText());
+        promfAl.sendKeys("Nguyen Viet Dung");
+        promfAl.accept();
+
+        String timeAlert = "timedAlert";
+        WebElement timeAlerElet = mWebDriver.findElement(By.id(timeAlert));
+        timeAlerElet.click();
+        waitForDebug(5000);
+        Alert timeAl = mWebDriver.switchTo().alert();
+        System.out.println("Text: " + confAl.getText());
+        timeAl.accept();
     }
 
-     /**
+    /**
      * Go to Test Website
      */
     private void gotoTestWebsite(Menu parent, Menu subMenu) {
