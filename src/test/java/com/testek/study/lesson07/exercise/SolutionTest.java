e03772dfb00a7db4ffb703586d2c28b7a16b3b23
Vũ Minh Hiệp <vuhiep465@gmail.com>
2025-06-05 23:06:16 +0700

    [Minh_Hiệp] HomeWork Lesson 7

--- src/test/java/com/testek/study/lesson07/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson07/exercise/SolutionTest.java
@@ -1,11 +1,10 @@
 package com.testek.study.lesson07.exercise;
 
 import com.testek.study.common.Menu;
+import io.cucumber.java.cs.Ale;
 import lombok.Getter;
 import lombok.Setter;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
@@ -45,19 +44,29 @@ public class SolutionTest {
      * URL: <a href="https://testek.vn/lab/auto/web-elements">...</a>
      * Access to Elements navigation
      */
-    @Test(description = "Element Interaction: Student Form")
-    public void testStudentForm() {
-        gotoTestWebsite(Menu.FORM, Menu.STUDENT_FORM);
-
-        // Handle your code here
-
-    }
 
     @Test(description = "Element Interaction: Small Frame")
     public void testSmallFrame() {
         gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.FRAME);
 
         // Handle your code here
+        //Find and access to small frame
+        String frmSmallID = "small-frame";
+        WebElement frmSmallEle = mWebDriver.findElement(By.id(frmSmallID));
+        mWebDriver.switchTo().frame(frmSmallEle);
+
+        //get text in small frame
+        WebElement lblTitleEle = mWebDriver.findElement(By.tagName("h1"));
+        String title = lblTitleEle.getText();
+        WebElement lblTextEle = mWebDriver.findElement(By.tagName("p"));
+        String text = lblTextEle.getText();
+
+        //Print text
+        System.out.println("Title: " + title);
+        System.out.println("Text: " + text);
+
+        //Back to default content
+        mWebDriver.switchTo().defaultContent();
     }
 
     @Test(description = "Element Interaction: Alert")
@@ -65,6 +74,26 @@ public class SolutionTest {
         gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.ALERT);
 
         // Handle your code here
+        String FORM_XPATH_BUTTON = "//button[@id='%s']";
+
+        String btnConfirmAlert = String.format(FORM_XPATH_BUTTON, "confirmAlert");
+        WebElement btnConfirmAlertELe = mWebDriver.findElement(By.xpath(btnConfirmAlert));
+        btnConfirmAlertELe.click();
+        Alert confirmAlert = mWebDriver.switchTo().alert();
+        waitForDebug(5000);
+        System.out.println("Confirm alert text : " + confirmAlert.getText());
+        confirmAlert.accept();
+
+        String btnPromptAlert = String.format(FORM_XPATH_BUTTON, "promptAlert");
+        WebElement btnPromptAlertEle = mWebDriver.findElement(By.xpath(btnPromptAlert));
+        btnPromptAlertEle.click();
+        Alert promptAlert = mWebDriver.switchTo().alert();
+        System.out.println("Prompt alert text : " + promptAlert.getText());
+        waitForDebug(5000);
+        promptAlert.sendKeys("Minh Hiep");
+        waitForDebug(5000);
+        promptAlert.accept();
+
     }
 
      /**
@@ -97,5 +126,14 @@ public class SolutionTest {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    public void testWindowsInteraction(){
+        mWebDriver.get("https://testek.vn/lab/auto/web-elements/");
+        waitForDebug(3000);
+        String homePageID = mWebDriver.getWindowHandle();
+
+
     }
 }
