a06cfed4fa54056a6a2e7cab6c9a11c2b74a7a66
lienltb <lienltb8@gmail.com>
2025-06-08 10:38:33 +0700

    [Lienltb8] Update BTVN lesson 7 (fix timed alert and interaction windows with js)

--- src/test/java/com/testek/study/lesson07/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson07/exercise/SolutionTest.java
@@ -1,8 +1,6 @@
 package com.testek.study.lesson07.exercise;
 
 import com.testek.study.common.Menu;
-import lombok.Getter;
-import lombok.Setter;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,12 +10,10 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.Objects;
-import java.util.Set;
 
 import static java.lang.Thread.sleep;
 
-@Getter
-@Setter
+
 public class SolutionTest {
     // The driver for interacting with the webpage
     private WebDriver mWebDriver;
@@ -99,42 +95,71 @@ public class SolutionTest {
     public void testInteractionWindows() {
         // 1. Open Menu Page
         gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.FRAME);
+        WebElement ltbTitleSubMenuPageEle = mWebDriver.findElement(By.id("frames-title"));
+        String actTitleSubMenuPageText = ltbTitleSubMenuPageEle.getText();
+        System.out.println("Title Sub Menu Page: " + actTitleSubMenuPageText);
 
-        // Handle your code here
         // Save the first window ID
         String firstWindowID = mWebDriver.getWindowHandle();
 
-        // 2.Open new tab and go to Home Page: https://testek.vn/lab/auto/web-elements/
+        // 2.Open new tab and go to Home Page
         mWebDriver.switchTo().newWindow(WindowType.TAB);
         mWebDriver.get("https://testek.vn/lab/auto/web-elements/");
-        waitForDebug(5000);
+        waitForDebug(2000);
+
+        // Verify switch Home Page
+        WebElement ltbTitleHomePageEle = mWebDriver.findElement(By.xpath("//b[normalize-space(.)='TESTEK - KIỂM THỬ THỰC CHIẾN']"));
+        String actTitleHomePageText = ltbTitleHomePageEle.getText();
+        System.out.println("Title Home Page: " + actTitleHomePageText);
+        String expTitleHomePageText = "TESTEK - KIỂM THỬ THỰC CHIẾN";
+        Assert.assertEquals(actTitleHomePageText, expTitleHomePageText,
+                "The title of page is not same as expected");
 
         // 3. Back to the before TAB: Menu Page
         mWebDriver.switchTo().window(firstWindowID);
-        waitForDebug(5000);
+        waitForDebug(2000);
 
-        // 4. Print the list of all windows currently open
-        Set<String> windowList = mWebDriver.getWindowHandles();
-        System.out.println("List of the first window: ");
-        for (String win : windowList) {
-            System.out.println(win);
-        }
+        // Verify switch Menu Page
+        System.out.println("Title Sub Menu Page: " + actTitleSubMenuPageText);
+        String expTitleSubMenuPageText = "Frames Example";
+        Assert.assertEquals(actTitleSubMenuPageText, expTitleSubMenuPageText,
+                "The title of page is not same as expected");
 
-        // 5. Open a new window and access Home page of Testek and menu page in 2 tabs
+
+        // 4. Open a new window and access Home page of Testek and menu page in 2 tabs
         mWebDriver.switchTo().newWindow(WindowType.WINDOW);
-        // Go to Menu Page
+        // Switch Menu Page
         gotoTestWebsite(Menu.ALERT_FRAME_AND_WINDOWS, Menu.FRAME);
         String secondWindowID = mWebDriver.getWindowHandle();
         System.out.println("The second window: " + secondWindowID);
+        waitForDebug(2000);
+
+        // Verify switch Menu Page
+        System.out.println("Title Sub Menu Page: " + actTitleSubMenuPageText);
+        Assert.assertEquals(actTitleSubMenuPageText, expTitleSubMenuPageText,
+                "The title of page is not same as expected");
+
+//        mWebDriver.switchTo().window(secondWindowID);
+//        mWebDriver.switchTo().newWindow(WindowType.TAB);
+        // Switch to Home Page
+        ((JavascriptExecutor) mWebDriver).executeScript("window.open('https://testek.vn/lab/auto/web-elements/', '_blank');");
+//        mWebDriver.get("https://testek.vn/lab/auto/web-elements/");
         waitForDebug(5000);
 
+        // Verify switch Home Page
+        System.out.println("Title Home Page: " + actTitleHomePageText);
+        Assert.assertEquals(actTitleHomePageText, expTitleHomePageText,
+                "The title of page is not same as expected");
+
+        //5. Switch to Menu Page
         mWebDriver.switchTo().window(secondWindowID);
-        mWebDriver.switchTo().newWindow(WindowType.TAB);
-        // Go to Home Page
-        mWebDriver.get("https://testek.vn/lab/auto/web-elements/");
         waitForDebug(5000);
         System.out.println("The current window: " + mWebDriver.getWindowHandle());
 
+        // Verify switch Menu Page
+        System.out.println("Title Sub Menu Page: " + actTitleSubMenuPageText);
+        Assert.assertEquals(actTitleSubMenuPageText, expTitleSubMenuPageText,
+                "The title of page is not same as expected");
 
     }
 
@@ -178,14 +203,14 @@ public class SolutionTest {
 
         // Press Cancel button
         confirmAlert.dismiss();
-        waitForDebug(5000);
+        waitForDebug(2000);
 
         // 3. Click "Prompt Alert"
         String promptAlertXPath = "//button[@id='promptAlert']";
         WebElement promptAlertEle = mWebDriver.findElement(By.xpath(promptAlertXPath));
         promptAlertEle.click();
         Alert promptAlert = mWebDriver.switchTo().alert();
-        waitForDebug(3000);
+        waitForDebug(1000);
 
         // Print the text of the prompt alert and verify
         String actualPromptAlertText = promptAlert.getText();
@@ -197,6 +222,7 @@ public class SolutionTest {
         // Type your name into the prompt alert
         String yourName = "Lien test";
         promptAlert.sendKeys(yourName);
+
         System.out.println("Your name: " + yourName);
         waitForDebug(3000);
 
@@ -208,6 +234,7 @@ public class SolutionTest {
         String timedAlertXPath = "//button[@id='timedAlert']";
         WebElement timedAlertEle = mWebDriver.findElement(By.xpath(timedAlertXPath));
         timedAlertEle.click();
+        waitForDebug(5000);
         Alert timedAlert = mWebDriver.switchTo().alert();
 
         // Print the text of the timed alert and verify
