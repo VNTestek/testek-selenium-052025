6229410fb89b8a344f4c06d147b69cacc56215d1
huongquynh <quynhhuongdo312@gmail.com>
2025-06-15 19:58:31 +0700

    huongquynh nop BTVN 10

--- src/test/java/com/testek/study/lesson10/exercise/SolutionTest.java
+++ src/test/java/com/testek/study/lesson10/exercise/SolutionTest.java
@@ -1,26 +1,34 @@
 package com.testek.study.lesson10.exercise;
 
-import com.testek.study.common.Menu;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 import static java.lang.Thread.sleep;
 
 @Getter
 @Setter
+@Slf4j
 public class SolutionTest {
     // The driver for interacting with the webpage
     private WebDriver mWebDriver;
+    private WebDriverWait mWebDriverWait;
+
 
     /**
      * Method will be executed before each class, configure Chrome Driver and initialize
@@ -31,6 +39,7 @@ public class SolutionTest {
         chromeOptions.addArguments("--max-window-size");
         chromeOptions.addArguments("--remote-allow-origins=*");
         mWebDriver = new ChromeDriver(chromeOptions);
+        mWebDriverWait = new WebDriverWait(mWebDriver, Duration.ofSeconds(10));
     }
 
     /**
@@ -38,6 +47,7 @@ public class SolutionTest {
      **/
     @AfterClass
     public void afterClass() {
+
         if (Objects.nonNull(mWebDriver)) mWebDriver.quit();
     }
 
@@ -46,32 +56,133 @@ public class SolutionTest {
      * Access to @link{Menu#FORM} navigation
      */
     @Test(description = "Element Interaction")
-    public void testStudentForm() {
-        gotoTestWebsite(Menu.FORM, Menu.STUDENT_FORM);
+    public void testAddNewProduct() {
 
         // Handle your code here
-    }
 
+        //Login site
+        login("admin_com_role", "aA12345678@");
+
+        //Access Menu > Sản phẩm
+        String subMnuProduct = "//div[@class='flex items-center']//div[contains(text(),'Sản phẩm')]";
+        clickTo(subMnuProduct, "Sản Phẩm");
+
+        //Click on [Them san pham] btn
+        String addProd = "//button[@testek='btn-add']";
+        clickTo(addProd, "Thêm sản phẩm");
+
+        //Get timestamp
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+
+        //Select 'Máy giặt' from [Danh mục]
+        String categoryDrpId = "form_item_category";
+        clickOnElement(categoryDrpId);
+        String categoryOptionXPath = "//td[@class='td-item td-item-category' and normalize-space(text())='Máy giặt']";
+        clickTo(categoryOptionXPath, "Danh Mục");
+
+        //Select 'LG VietNam' from [Nhà cung cấp]
+        String supplierDrpId = "form_item_supplier";
+        clickOnElement(supplierDrpId);
+
+        String supplierOptionXPath = "//div[@class='ant-select-item-option-content']//td[@class='td-item td-item-supplier' and text() = 'LG VietNam']";
+        clickTo(supplierOptionXPath, "Nhà cung cấp");
+
+
+        //Input Mã
+        String productCode = "AUTO202505_LG_QUYNH" + timestamp;
+        inputValueById("form_item_code", productCode, "Mã");
+
+        //Input Tên
+        inputValueById("form_item_name", "Máy giặt AUTO202505_LG_QUYNH" + timestamp, "Tên");
+
+        //Input Đơn vị
+        inputValueById("form_item_unit", "Chiếc", "Đơn vị");
+
+        //Input Mô tả
+        inputValueById("form_item_description", "Máy giặt LG - Được xây dựng bởi AUTO202505_LG_QUYNH" + timestamp, "Mô tả");
+
+        //Input Gía
+        inputValueById("form_item_price", "10000000", "Giá");
+
+        //Input Số lượng
+        inputValueById("form_item_quantity", "10", "Số lượng");
+
+
+        inputValueById("form_item_quantity", "10", "Số lượng");
+
+        //Click on [Thêm] button
+        String btnAddXPath = "//button[@testek='btn-add']";
+        clickTo(btnAddXPath, "Thêm");
+
+        //Print info [Mã sản phẩm]
+        inputValueByXPath("//input[@placeholder='Mã sản phẩm']", productCode, "Mã sản phẩm");
+        printInfo("//input[@placeholder='Mã sản phẩm']", "Mã sản phẩm");
+
+        //View details
+        String viewDetailsXPath = "//button[@testek='btn-view-detail']";
+        clickTo(viewDetailsXPath, "Xem chi tiết");
+
+        //Print info [Error message]
+        printInfo("//div[@testek='textarea']", "Error message");
+
+    }
 
 
     /**
      * Go to Test Website
      */
-    private void gotoTestWebsite(Menu parent, Menu subMenu) {
-        String url = "https://testek.vn/lab/auto/web-elements/";
+    private void login(String username, String password) {
+        String url = "https://testek.vn/lab/auto/login";
         mWebDriver.get(url);
 
-        String XPATH_MENU_FORMAT = "//div[@test-id='%s']";
+        //Input username, pwd
+        WebElement usernameEle = mWebDriver.findElement(By.id("normal_login_username"));
+        usernameEle.sendKeys(username);
+        WebElement passwordEle = mWebDriver.findElement(By.id("normal_login_password"));
+        passwordEle.sendKeys(password);
 
-        // Access: Form > [Student Form]
-        WebElement mnuElementEle = mWebDriver.findElement(By.xpath(String.format(XPATH_MENU_FORMAT, parent.getName())));
-        mnuElementEle.click();
-
-        WebElement mnuEle = mWebDriver.findElement(By.xpath(String.format(XPATH_MENU_FORMAT, subMenu.getName())));
-        mnuEle.click();
-
-        waitForDebug(5000);
+        //Login
+        String btnLoginXPath = "//span[contains(text(),'Đăng nhập')]";
+        clickTo(btnLoginXPath, "Đăng nhập");
     }
+
+    private void clickTo(String xpath, String title) {
+
+        WebElement element = mWebDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+        element.click();
+        log.info("Clicked on [{}]", title);
+
+    }
+
+    private void clickOnElement(String id) {
+        WebElement element = mWebDriver.findElement(By.id(id));
+        element.click();
+
+    }
+
+    private void inputValueById(String id, String value, String title) {
+        WebElement element = mWebDriver.findElement(By.id(id));
+        element.clear();
+        element.sendKeys(value);
+        log.info("Input: [{}] to [{}]", value, title);
+    }
+
+    private void inputValueByXPath(String xpath, String value, String title) {
+        WebElement element = mWebDriver.findElement(By.xpath(xpath));
+        element.clear();
+        element.sendKeys(value);
+        log.info("Input: [{}] to [{}]", value, title);
+    }
+
+    private void printInfo(String xpath, String title) {
+        WebElement skuElement = mWebDriver.findElement(By.xpath(xpath));
+        String value = skuElement.getAttribute("value");
+        if (Objects.nonNull(value)) {
+            log.info("Result: [{}] to [{}]", title, value);
+        }
+    }
+
 
     /**
      * Sleep for debugging
@@ -85,4 +196,6 @@ public class SolutionTest {
             throw new RuntimeException(e);
         }
     }
+
+
 }
